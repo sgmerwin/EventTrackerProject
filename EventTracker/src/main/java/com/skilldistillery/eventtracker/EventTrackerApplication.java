014708925d98060ad96e864e0b736c6c9ff89c33
sgmerwin <stevegmerwin@gmail.com>
2020-02-09 10:18:03 -0700

    readme

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
+++ EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventTracker1Application {
+public class EventTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventTracker1Application.class, args);
+		SpringApplication.run(EventTrackerApplication.class, args);
 	}
 
 }
