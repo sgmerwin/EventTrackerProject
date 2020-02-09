abe18ca4459b301b469811ce4eabddb191b0bea3
sgmerwin <stevegmerwin@gmail.com>
2020-02-09 10:22:39 -0700

    readme

--- EventTracker-2/src/main/java/com/skilldistillery/eventtracker/repositories/GasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.eventtracker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skilldistillery.eventtracker.entities.GasTracker;
-
-public interface GasRepository extends JpaRepository<GasTracker, Integer> {
-
-}
